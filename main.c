4e2a81bff55d0f98408d045414f7f97d5d1d2f4d
Simon Krix <simon@smonson.com>
2021-04-04 16:32:09 +1000

    Added quanta mode

--- main.c
+++ main.c
@@ -56,6 +56,19 @@ const uint8_t heat_string[] PROGMEM = {
   LCD_H, LCD_E, LCD_A, LCD_T, LCD_SPACE, LCD_O, LCD_N, LCD_END
 };
 
+const uint8_t mode_string[] PROGMEM = {
+  LCD_M, LCD_O, LCD_D, LCD_E, LCD_COLON, LCD_END
+};
+
+const uint8_t toggle_string[] PROGMEM = {
+  LCD_T, LCD_O, LCD_G, LCD_G, LCD_L, LCD_E, LCD_END
+};
+
+const uint8_t quanta_string[] PROGMEM = {
+  LCD_Q, LCD_U, LCD_A, LCD_N, LCD_T, LCD_A, LCD_END
+};
+
+
 uint8_t heating = 0;
 
 int main()
@@ -70,6 +83,11 @@ int main()
 	//video_vline(20, 20, 40, 1);
 
 	uint8_t count = 0;
+	enum {
+		MODE_TOGGLE,
+		MODE_QUANTA,
+	} mode = MODE_TOGGLE;
+
 	while (1) {
 		uint16_t temp = max6675_read_raw();
 
@@ -94,6 +112,14 @@ int main()
 		drawletter(31 + 28, 0, LCD_DEGREES);
 		drawletter(31 + 32, 0, LCD_C);
 
+		// Mode
+		drawstring(0, 24, mode_string);
+		if (mode == MODE_TOGGLE) {
+			drawstring(32, 24, toggle_string);
+		} else if (mode == MODE_QUANTA) {
+			drawstring(32, 24, quanta_string);
+		}
+
 		// AVR internal core temperature sensor
 		print_num(74, 0, temp2, 3, 0);
 		drawletter(74 + 15, 0, LCD_DEGREES);
@@ -104,40 +130,68 @@ int main()
 		if (heating) {
 			drawstring(0, 11, heat_string);
 
-		  	// Draw the heating time
-			cli();
-			// Get the 125Hz clock
-			uint32_t ticks_copy = ticks;
-			sei();
-			ticks_copy /= 125;
-			print_num(105, 0, ticks_copy / 60, 2, 0);
-			drawletter(115, 0, LCD_COLON);
-			print_num(118, 0, ticks_copy % 60, 2, 1);
 		}
 
-		print_num(0, 49, count++, 5, 0);
+		// Draw the heating/cooling time
+		cli();
+		// Get the 125Hz clock
+		uint32_t ticks_copy = ticks;
+		sei();
+		ticks_copy /= 125;
+		print_num(105, 0, ticks_copy / 60, 2, 0);
+		drawletter(115, 0, LCD_COLON);
+		print_num(118, 0, ticks_copy % 60, 2, 1);
+
+		//print_num(0, 49, count++, 5, 0);
 
 		// Simulated buttons
-		drawletter(64, 38, button_flags & BUTTON_1 ? LCD_O : LCD_PERIOD);
-		drawletter(64 + 10, 38, button_flags & BUTTON_2 ? LCD_O : LCD_PERIOD);
+		//drawletter(64, 38, button_flags & BUTTON_1 ? LCD_O : LCD_PERIOD);
+		//drawletter(64 + 10, 38, button_flags & BUTTON_2 ? LCD_O : LCD_PERIOD);
 		// drawletter(64 + 20, 38, button_flags & BUTTON_3 ? LCD_O : LCD_PERIOD);
 
 		ssd1306_update();
 
 		// Handle events --------------------------------
 
-		// TODO: this is better as a toggle on/off
-		// Start
-		if (test_button(BUTTON_1, 0)) {
-			heating = 1;
-			RELAY_PORT_REG |= _BV(RELAY_PIN);
-			clear_clock();
+		if (mode == MODE_TOGGLE) {
+			// Start/stop
+			if (test_button(BUTTON_1, 0)) {
+				heating = heating ? 0 : 1;
+				if (heating) {
+					RELAY_PORT_REG |= _BV(RELAY_PIN);
+				} else {
+					RELAY_PORT_REG &= ~_BV(RELAY_PIN);	
+				}
+				clear_clock();
+			}
+		} else if (mode == MODE_QUANTA) {
+			// 10s heater "burn"
+			if (test_button(BUTTON_1, 0) && heating == 0) {
+				RELAY_PORT_REG |= _BV(RELAY_PIN);
+				heating = 1;
+				count = 20;
+				clear_clock();
+			}
+
+			if (heating) {
+				if (count == 0) {
+					heating = 0;
+					RELAY_PORT_REG &= ~_BV(RELAY_PIN);	
+					clear_clock();
+				} else {
+					count -= 1;
+				}
+			}
 		}
 
 		// Stop
 		if (test_button(BUTTON_2, 1)) {
-			heating = 0;
-			RELAY_PORT_REG &= ~_BV(RELAY_PIN);
+			// mode button
+			if (mode == MODE_TOGGLE) {
+				mode = MODE_QUANTA;
+			} else {
+				mode = MODE_TOGGLE;
+			}
 		}
 
 
